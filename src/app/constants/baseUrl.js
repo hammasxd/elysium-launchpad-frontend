9fd1fd9f27aafb6ce907c9e803e51a0a125be66d
hammasxd <hammas.ali@vaivaltech.com>
2023-11-07 12:34:53 +0500

    base url fix

--- src/app/constants/baseUrl.js
+++ src/app/constants/baseUrl.js
@@ -1,7 +1,7 @@
 module.exports = {
 
    // baseUrl: "http://127.0.0.1:5008/api",
-   baseUrl: "https://elysium-launchpad-adminbackend-dev.vulcanforged.com/api",
+   baseUrl: "https://elysium-launchpad-adminbackend-nft-dev.vulcanforged.com/api",
    // baseUrl: "https://elysium-launchpad-adminbackend-nft-dev.vulcanforged.com/api",
    // baseUrl: "http://localhost:5008/api",
    saleToken: "EUSD",
